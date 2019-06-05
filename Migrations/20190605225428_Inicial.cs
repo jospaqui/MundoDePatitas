48db5524bd5f90560eff72122bc67579da7013dc
MauricioFalla <luis.mauricio.falla@gmail.com>
2019-06-05 17:55:33 -0500

    Agreganado la migracion y la BASE DE DATOS

---
+++ Migrations/20190605225428_Inicial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MundoDePatitas.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tipos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tipos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mascotas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false),
+                    Adoptado = table.Column<bool>(nullable: false),
+                    edad = table.Column<int>(nullable: false),
+                    sexo = table.Column<bool>(nullable: false),
+                    Foto = table.Column<string>(nullable: false),
+                    TipoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascotas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mascotas_Tipos_TipoId",
+                        column: x => x.TipoId,
+                        principalTable: "Tipos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mascotas_TipoId",
+                table: "Mascotas",
+                column: "TipoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mascotas");
+
+            migrationBuilder.DropTable(
+                name: "Tipos");
+        }
+    }
+}
