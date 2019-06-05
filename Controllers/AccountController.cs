2cb1a77dee46d014078d5b0c2cdf3d0bfeff9424
AdeGabriela1 <adelaotayzacp@gmail.com>
2019-06-05 17:58:19 -0500

    controllers y views

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,30 +5,64 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MundoDePatitas.Datos;
 using MundoDePatitas.Models;
 namespace MundoDePatitas.Controllers
 {
     public class AccountController:Controller
     {
-    public IActionResult Login()
-    {
-      //TODO: Implement Realistic Implementation
-      return View();
-    }
-    public IActionResult Register()
-    {
-      //TODO: Implement Realistic Implementation
-      return View();
-    }
-    public IActionResult Confirmacion()
-    {
-      //TODO: Implement Realistic Implementation
-      return View();
-    }
+  
+ 
+ 
     public IActionResult RecuperarContraseña()
     {
       //TODO: Implement Realistic Implementation
       return View();
     }
-    }
+
+     private MascotaContext _context; //el contexto 
+
+        public AccountController (MascotaContext c){ //el constructor del contexto
+            _context = c;
+
+        }
+
+        //ESTA ES LA FICHA DE REGISTRO
+        public IActionResult RegistroUsuarios(){
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult RegistroUsuarios(string correo, string nombre, string pass, string direccion){
+            TempData["correo"]=correo;
+            TempData["nombre"]=nombre;
+            TempData["pass"]=pass;
+            TempData["direccion"]=direccion;
+        
+         return RedirectToAction("ConfirmarRegistro");
+        }
+
+        public IActionResult ConfirmarRegistro(){
+            return View();
+        }
+
+
+
+         public IActionResult Login(){
+            return View();
+         }
+        
+         public IActionResult Login(string u, string p){
+              //  if(u=="") aca el condicional de correo y clave 
+            
+            ViewBag.Error="Datos Incorrectos";
+            return View();
+        }
+        
+
+
+
+
+
+    }    
 }
