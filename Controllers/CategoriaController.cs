b97b0102520d2203d9c218bf04b90dec8af64b65
MauricioFalla <luis.mauricio.falla@gmail.com>
2019-06-16 22:22:12 -0500

    Agregando AgregarEspecies, ListadoEspieces y Actualizando CategoriaController

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MundoDePatitas.Datos;
 using MundoDePatitas.Models;
 namespace MundoDePatitas.Controllers
@@ -17,6 +18,42 @@ namespace MundoDePatitas.Controllers
             _context = c;
 
       }
+    
+    public IActionResult ListadoEspecie( int especieID) //Para listar todas las especies
+    {
+      ViewBag.especie = _context.Especies.ToList();
+
+      var especie = _context.Especies.Include(x => x.Id).ToList();
+
+      if(especieID != 0)
+      {
+        especie = _context.Especies.Include(x => x.Id).Where(x => x.Id == especieID).ToList();
+      }
+      return View(especie);
+    }
+
+
+
+    public IActionResult AgregarEspecie() //Para el registro de nuevas especies
+    {
+      ViewBag.especie = _context.Especies.ToList();   
+      return View();
+    }
+    [HttpPost] //la parte del posteo
+    public IActionResult AgregarEspecie( Especie e)
+    {
+      if (ModelState.IsValid)
+      {
+          _context.Add(e);
+          _context.SaveChanges();
+          return RedirectToAction("ListadoEspecie");
+      }
+      ViewBag.especie = _context.Especies.ToList();
+      return View(e);
+    }
+
+
+
     public IActionResult Mascotas()
     {
       //TODO: Implement Realistic Implementation
